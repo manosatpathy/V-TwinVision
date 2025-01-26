d5620246f7a90b3c81a2315d07691da85061a5c9
manosatpathy <manosatpathy@gmail.com>
2025-01-26 23:24:58 +0530

    chore: Setup React Query

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider defaultTheme="dark">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<WeatherDashboard />} />
-          <Route path="/city/:cityName" element={<CityPage />} />
-        </Routes>
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
