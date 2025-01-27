4a4f157aee078349fa31565584f595d0ea923b46
manosatpathy <manosatpathy@gmail.com>
2025-01-27 23:37:05 +0530

    chore: Remove unused logo image from the file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,11 +8,7 @@ const Header = () => {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to="/home">
-          <img
-            src={theme === "dark" ? "/logo.png" : "logo2.png"}
-            alt="logo"
-            className="h-14"
-          />
+          <img src="/logo.png" alt="logo" className="h-14" />
         </Link>
         <div>
           {/* search */}
