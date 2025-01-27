c901895b065c11cf3e735ffa37429c02c15b1b3e
manosatpathy <manosatpathy@gmail.com>
2025-01-27 23:33:54 +0530

    feat: configure default options for React Query

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+        gcTime: 10 * 60 * 1000,
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark">
