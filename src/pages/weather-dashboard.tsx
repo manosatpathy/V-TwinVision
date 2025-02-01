fef0957cdb78587281f71a2c0f1b7dd18dccd323
manosatpathy <manosatpathy@gmail.com>
2025-02-02 00:00:18 +0530

    feat: add weather forecast component

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,23 +1,25 @@
-import CurrentWeather from "@/components/current-weather";
-import HourlyTemperature from "@/components/hourly-temperature";
-import WeatherSkeleton from "@/components/loading-skeleton";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import WeatherDetails from "@/components/weather-details";
+import WeatherSkeleton from "@/components/weather-skeleton";
 import { useGeolocation } from "@/hooks/use-geolocation";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   useForecastQuery,
   useReverseGeocodeQuery,
   useWeatherQuery,
 } from "@/hooks/use-weather";
-import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react";
-
+import CurrentWeather from "@/components/current-weather";
+import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
+import FavoriteCities from "@/components/favorite-cities";
+import HourlyTemperature from "@/components/hourly-temperature";
 const WeatherDashboard = () => {
   const {
     coordinates,
     error: locationError,
     getLocation,
-    isLoading: locationLoading,
+    isLoading: loadingLocation,
   } = useGeolocation();
 
   const locationQuery = useReverseGeocodeQuery(coordinates);
@@ -26,17 +28,17 @@ const WeatherDashboard = () => {
 
   const handleRefresh = () => {
     getLocation();
+
     if (coordinates) {
       weatherQuery.refetch();
-      forecastQuery.refetch();
       locationQuery.refetch();
+      forecastQuery.refetch();
     }
   };
 
-  if (locationLoading) {
+  if (loadingLocation) {
     return <WeatherSkeleton />;
   }
-
   if (locationError) {
     return (
       <Alert variant="destructive">
@@ -58,10 +60,10 @@ const WeatherDashboard = () => {
       <Alert>
         <MapPin className="h-4 w-4" />
         <AlertTitle>Location Required</AlertTitle>
-        <AlertDescription className="flex flex-col gap-4">
-          <p>Please enable location access to see your local weather.</p>
-          <Button variant="outline" onClick={getLocation} className="w-fit">
-            <MapPin className="mr-2 h-4 w-4" />
+        <AlertDescription>
+          <p>{locationError}</p>
+          <Button onClick={getLocation}>
+            <MapPin className="w-4 h-4 mr-2" />
             Enable Location
           </Button>
         </AlertDescription>
@@ -76,11 +78,11 @@ const WeatherDashboard = () => {
       <Alert variant="destructive">
         <AlertTriangle className="h-4 w-4" />
         <AlertTitle>Error</AlertTitle>
-        <AlertDescription className="flex flex-col gap-4">
-          <p>Failed to fetch weather data. Please try again.</p>
-          <Button onClick={handleRefresh} variant="outline" className="w-fit">
-            <RefreshCcw className="mr-2 h-4 w-4" />
-            retry
+        <AlertDescription>
+          <p>Failed to Fetch Weather Data Please Try Again</p>
+          <Button onClick={getLocation}>
+            <RefreshCw onClick={handleRefresh} className="w-4 h-4 mr-2" />
+            Retry
           </Button>
         </AlertDescription>
       </Alert>
@@ -93,16 +95,17 @@ const WeatherDashboard = () => {
 
   return (
     <div className="space-y-4">
-      {/* Favorite Cities */}
+      <FavoriteCities />
+
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
         <Button
-          variant="outline"
-          size="icon"
+          variant={"outline"}
+          size={"icon"}
           onClick={handleRefresh}
           disabled={weatherQuery.isFetching || forecastQuery.isFetching}
         >
-          <RefreshCcw
+          <RefreshCw
             className={`h-4 w-4 ${
               weatherQuery.isFetching ? "animate-spin" : ""
             }`}
@@ -118,9 +121,10 @@ const WeatherDashboard = () => {
           />
           <HourlyTemperature data={forecastQuery.data} />
         </div>
-        <div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
           <WeatherDetails data={weatherQuery.data} />
-          {/* forecast */}
+          <WeatherForecast data={forecastQuery.data} />
         </div>
       </div>
     </div>
