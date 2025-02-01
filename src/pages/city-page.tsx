2524a43e209be65c70fd62954cfdf4bcc6c24084
manosatpathy <manosatpathy@gmail.com>
2025-02-02 00:01:59 +0530

    feat: Add CityPage to display current weather, forecast, and favorites

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,5 +1,68 @@
+import CurrentWeather from "@/components/current-weather";
+import FavoriteButton from "@/components/favorite-button";
+import HourlyTemperature from "@/components/hourly-temperature";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
+import WeatherSkeleton from "@/components/weather-skeleton";
+import { useForecastQuery, useWeatherQuery } from "@/hooks/use-weather";
+import { AlertTriangle } from "lucide-react";
+import { useParams, useSearchParams } from "react-router-dom";
+
 const CityPage = () => {
-  return <div>City page</div>;
+  const [searchParams] = useSearchParams();
+
+  const params = useParams();
+  const lat = parseFloat(searchParams.get("lat") || "0");
+  const lon = parseFloat(searchParams.get("lon") || "0");
+
+  const coordinates = { lat, lon };
+
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+
+  if (weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          <p>Failed to Fetch Weather Data Please Try Again</p>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!weatherQuery.data || !forecastQuery.data || !params.cityName) {
+    return <WeatherSkeleton />;
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">
+          {params.cityName},{weatherQuery.data.sys.country}
+        </h1>
+        <div>
+          <FavoriteButton
+            data={{ ...weatherQuery.data, name: params.cityName }}
+          />
+        </div>
+      </div>
+
+      <div className="grid gap-6">
+        <div className="flex flex-col lg:flex-row gap-2">
+          <CurrentWeather data={weatherQuery.data} />
+          <HourlyTemperature data={forecastQuery.data} />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
+          <WeatherDetails data={weatherQuery.data} />
+          <WeatherForecast data={forecastQuery.data} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CityPage;
