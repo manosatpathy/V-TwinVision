3ad04a4f5716d3b90672676df2dc8c31d88cae34
manosatpathy <manosatpathy@gmail.com>
2025-02-01 23:56:03 +0530

    feat: Add copyright text to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,16 @@
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 import Header from "./header";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
-      <Header />
+      <Header/>
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/30">
-        <div className="container mx-auto px-4 text-center text-gray-400">
-          <p>Made with 💓 by Mano Satpathy</p>
+      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
+        <div className="container mx-auto px-4 text-center text-gray-500">
+          <p>© 2024 Mano Satpathy. All rights reserved.</p>
         </div>
       </footer>
     </div>
