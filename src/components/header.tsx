c2560c56332caf35218c8b7aa07584fa71d2c2c5
manosatpathy <manosatpathy@gmail.com>
2025-02-01 23:53:20 +0530

    refactor: Add isDark variable to header for theme handling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,36 @@
 import { useTheme } from "@/context/theme-provider";
 import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
+import CitySearch from "./city-search";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link to="">
+        <Link to={"/"}>
           <img
-            src={theme === "dark" ? "/logo2.png" : "logo.png"}
+            src={isDark ? "/logo2.png" : "/logo.png"}
             alt="logo"
             className="h-14"
           />
         </Link>
-        <div>
-          {/* search */}
+
+        <div className="flex gap-4">
+          <CitySearch />
+
           <div
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             className={`flex items-center cursor-pointer transition-transform duration-500
-              ${theme === "dark" ? "rotate-180" : "rotate-0"}
-              `}
+                        ${isDark ? "rotate-180" : "rotate-0"}
+
+                    `}
+            onClick={() => setTheme(isDark ? "light" : "dark")}
           >
-            {theme === "dark" ? (
-              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all"></Sun>
             ) : (
-              <Moon className="h-6 w-6 text-blue-5 00 rotate-0 transition-all" />
+              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all"></Moon>
             )}
           </div>
         </div>
